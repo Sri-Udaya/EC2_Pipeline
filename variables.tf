8ae4af708bd3aebb5abaeff129e439a963866475
Sri-Udaya <110080016+Sri-Udaya@users.noreply.github.com>
2023-02-28 10:35:32 +1100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+	type = string
+	default = "us-east-1"
+}
+
+variable "access_key" {
+	type = string
+	defalult = "AKIAU3IF2SV5BFCQCKVU"
+}
+
+varibale "secret_key" {
+	type = string
+	default = "IprEwv/Bg93ayK2dWH5YknjXI7kEuYDKG2hzc2fb"
+}
